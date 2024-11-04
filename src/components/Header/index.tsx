294421b2f728c7c17cb2f97049790d5b422f9a30
Dawid Glapiak <dg18@live.it>
2024-11-04 10:36:50 +0100

    updated hero and navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
-// Codice aggiornato per disattivare tutti i link della navbar e cambiare il cursore
+"use client";
+
 import React, { useEffect, useState } from "react";
 import { signOut, useSession } from "next-auth/react";
 import { Menu } from "@/types/menu";
@@ -11,43 +12,17 @@ import GlobalSearchModal from "@/components/GlobalSearch";
 
 const menuData: Menu[] = [
   {
-    label: "Features",
+    label: "Leistungen",
     route: "/#features",
   },
   {
-    label: "About",
+    label: "Designs",
     route: "/#about",
   },
   {
-    label: "How It Works",
+    label: "Preise",
     route: "/#work-process",
   },
-  {
-    label: "Support",
-    route: "/#support",
-  },
-  // {
-  //   label: "Pages",
-  //   route: "#",
-  //   children: [
-  //     {
-  //       label: "Blog Grids",
-  //       route: "/blog",
-  //     },
-  //     {
-  //       label: "404 Error",
-  //       route: "/error",
-  //     },
-  //     {
-  //       label: "Sign In",
-  //       route: "/auth/signin",
-  //     },
-  //     {
-  //       label: "Sign Up",
-  //       route: "/auth/signup",
-  //     },
-  //   ],
-  // },
 ];
 
 const Header = () => {
@@ -64,13 +39,11 @@ const Header = () => {
   const { data: session } = useSession();
 
   const pathUrl = usePathname();
-  // Navbar toggle
   const [navbarOpen, setNavbarOpen] = useState(false);
   const navbarToggleHandler = () => {
     setNavbarOpen(!navbarOpen);
   };
 
-  // Sticky Navbar
   const [sticky, setSticky] = useState(false);
   const handleStickyNavbar = () => {
     if (window.scrollY >= 80) {
@@ -83,7 +56,6 @@ const Header = () => {
     window.addEventListener("scroll", handleStickyNavbar);
   });
 
-  // submenu handler
   const [openIndex, setOpenIndex] = useState(-1);
   const handleSubmenu = (index: number) => {
     if (openIndex === index) {
@@ -110,7 +82,7 @@ const Header = () => {
       >
         <div className="container relative max-w-[1400px]">
           <div className="flex items-center justify-between">
-            <div className="block py-4 lg:py-0">
+            <div className="block w-1/3 py-4 lg:py-0">
               <Link href="/" className="block max-w-[145px] sm:max-w-[180px]">
                 <span className="text-[30px] font-bold text-[#181C30] dark:text-white">
                   Webomo
@@ -143,65 +115,40 @@ const Header = () => {
               </button>
 
               <nav className="fixed left-0 top-0 z-[999] flex h-screen w-full items-center justify-center bg-white bg-opacity-95 text-center backdrop-blur-sm dark:bg-black dark:bg-opacity-95 lg:static lg:h-auto lg:w-max lg:bg-transparent lg:bg-opacity-100 lg:backdrop-blur-0 lg:backdrop-blur-none lg:dark:bg-transparent dark:lg:bg-opacity-100">
-                <ul className="items-center space-y-3 lg:flex lg:space-x-8 lg:space-y-0 xl:space-x-10">
-                  {menuData.map((item, index) =>
-                    item.children ? (
-                      <li
-                        key={index}
-                        className="submenu-item menu-item group relative"
+                <ul className=" items-center space-y-3 lg:flex lg:space-x-12 lg:space-y-0 xl:space-x-14">
+                  {menuData.map((item, index) => (
+                    <li key={index} className="menu-item">
+                      <Link
+                        href="#"
+                        onClick={(e) => e.preventDefault()}
+                        className={`${sticky ? "lg:py-[21px]" : "lg:py-7"} ud-menu-scroll pointer-events-none inline-flex cursor-not-allowed items-center text-base font-light text-black hover:text-primary dark:text-white dark:hover:text-primary`}
                       >
-                        <Link
-                          onClick={(e) => e.preventDefault()} // Disables click
-                          href="#"
-                          className={`${sticky ? "lg:py-[21px]" : "lg:py-7"} submenu-taggler pointer-events-none inline-flex cursor-not-allowed items-center text-base font-medium text-black hover:text-primary group-hover:text-primary dark:text-white dark:hover:text-primary`}
-                        >
-                          {item.label}
-                          <span className="pl-3">
-                            <svg
-                              width="14"
-                              height="8"
-                              viewBox="0 0 14 8"
-                              className={`fill-current ${openIndex === index ? "rotate-180 lg:rotate-0" : ""}`}
-                            >
-                              <path d="M6.54564 5.09128L11.6369 0L13.0913 1.45436L6.54564 8L0 1.45436L1.45436 0L6.54564 5.09128Z" />
-                            </svg>
-                          </span>
-                        </Link>
+                        {item.label}
+                      </Link>
+                    </li>
+                  ))}
 
-                        <ul
-                          className={`${openIndex === index ? "" : "hidden"} submenu space-y-5 pt-5 duration-300 lg:invisible lg:absolute lg:top-[120%] lg:block lg:w-[250px] lg:rounded-lg lg:bg-white lg:px-8 lg:pb-5 lg:text-left lg:opacity-0 lg:shadow-card lg:group-hover:visible lg:group-hover:top-full lg:group-hover:opacity-100 dark:lg:border-transparent dark:lg:bg-[#15182A] lg:dark:shadow-card-dark`}
-                        >
-                          {item.children.map((childItem, childIndex) => (
-                            <li key={childIndex}>
-                              <Link
-                                href="#"
-                                onClick={(e) => e.preventDefault()}
-                                className="font-heading pointer-events-none inline-flex cursor-not-allowed items-center justify-center text-center text-base text-black hover:text-primary dark:text-white dark:hover:text-primary"
-                              >
-                                {childItem.label}
-                              </Link>
-                            </li>
-                          ))}
-                        </ul>
-                      </li>
-                    ) : (
-                      <li key={index} className="menu-item">
-                        <Link
-                          href="#"
-                          onClick={(e) => e.preventDefault()}
-                          className={`${sticky ? "lg:py-[21px]" : "lg:py-7"} ud-menu-scroll pointer-events-none inline-flex cursor-not-allowed items-center text-base font-medium text-black hover:text-primary dark:text-white dark:hover:text-primary`}
-                        >
-                          {item.label}
-                        </Link>
-                      </li>
-                    ),
-                  )}
+                  {/* Kostenlose Beratung Button */}
+                  <li className="lg:ml-4">
+                    <Link
+                      href="#"
+                      className="inline-flex h-[60px] items-center rounded-2xl bg-indigo-600 px-[30px]  text-white hover:bg-opacity-90 dark:bg-indigo-500 dark:text-white dark:hover:bg-opacity-90 md:hidden"
+                    >
+                      Kostenlose Beratung
+                    </Link>
+                  </li>
                 </ul>
               </nav>
             </div>
 
-            <div className="mr-[60px] flex items-center justify-end lg:mr-0">
+            <div className="mr-[60px] flex items-center justify-end space-x-4 lg:mr-0">
               <DarkModeSwitcher />
+              <Link
+                href="#"
+                className="hidden  items-center rounded-2xl bg-indigo-600 px-4 py-2 text-white hover:bg-opacity-90 dark:bg-indigo-500 dark:text-white dark:hover:bg-opacity-90 md:inline-flex"
+              >
+                Kostenlose Beratung
+              </Link>
 
               {session ? (
                 <div className="hidden items-center sm:flex">
@@ -211,29 +158,18 @@ const Header = () => {
                   <button
                     aria-label="SignOut"
                     onClick={() => signOut()}
-                    className="rounded-md bg-primary px-[30px] py-[10px] text-base font-medium text-white hover:bg-opacity-90"
+                    className="rounded-md bg-primary px-4 py-2 text-base font-medium text-white hover:bg-opacity-90"
                   >
                     Sign Out
                   </button>
                 </div>
               ) : (
-                <>
-                  <Link
-                    href="#"
-                    onClick={(e) => e.preventDefault()}
-                    className="pointer-events-none hidden cursor-not-allowed px-6 py-[10px] text-base font-medium text-black hover:text-primary dark:text-white dark:hover:text-primary sm:inline-block"
-                  >
-                    Sign In
-                  </Link>
-
-                  <Link
-                    href="#"
-                    onClick={(e) => e.preventDefault()}
-                    className="pointer-events-none hidden cursor-not-allowed rounded-md bg-primary px-[30px] py-[10px] text-base font-medium text-white hover:bg-opacity-90 sm:inline-block"
-                  >
-                    Sign Up
-                  </Link>
-                </>
+                <Link
+                  href="#"
+                  className="hidden items-center rounded-2xl bg-black px-4 py-2 text-white hover:bg-opacity-90 dark:bg-white dark:text-black dark:hover:bg-opacity-90 lg:inline-flex"
+                >
+                  Login
+                </Link>
               )}
             </div>
           </div>
