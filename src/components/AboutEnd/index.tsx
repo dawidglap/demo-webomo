b29a3d48f1ca7ead8fbf29bc2a7cf208a1dc2471
Dawid Glapiak <dg18@live.it>
2024-11-14 21:13:14 +0100

    aboutend done

--- src/components/AboutEnd/index.tsx
+++ src/components/AboutEnd/index.tsx
@@ -12,13 +12,13 @@ const AboutEnd = () => {
       </h2>
 
       {/* Main Container with Rounded Background */}
-      <div className="relative flex w-full max-w-6xl flex-col items-center justify-between rounded-3xl bg-gradient-to-b from-[#ECE7FE] via-purple-200 to-[#B4A2E0] p-8 dark:from-slate-700 dark:to-black md:flex-row md:p-16">
+      <div className="relative flex w-full max-w-5xl flex-col items-center justify-between rounded-3xl bg-gradient-to-b from-[#ECE7FE] via-purple-200 to-[#B4A2E0] p-8 dark:from-slate-700 dark:to-black md:flex-row md:p-16">
         {/* Text and Call-to-Action */}
         <div className="text-center md:max-w-lg md:text-left lg:max-w-2xl">
           <h3 className="mb-4 text-2xl font-bold text-black dark:text-white">
             Mehr erreichen mit webomo
           </h3>
-          <p className="mb-6 text-black dark:text-white">
+          <p className="mb-6 text-body dark:text-slate-400 ">
             Wir steigern Emotionen bei deiner Zielgruppe.Deine Inhalte wie
             Bilder,Videos und Content werden in{" "}
             <span className="font-bold">Hollywood-Kinoqualität</span> erstellt.
@@ -32,22 +32,22 @@ const AboutEnd = () => {
 
         {/* iPhone Frame with Video - Positioned at the right side */}
         <div className="relative hidden md:block">
-          {/* <div className="relative  h-[234px] w-[108px] overflow-hidden">
+          <div className="relative  left-[100%] h-[234px] w-[108px] overflow-hidden ">
             <Image
               src="/images/screens/mobile-frame.png"
               alt="mobile frame"
               layout="fill"
               objectFit="cover"
-              className="absolute inset-0 z-10"
+              className="absolute inset-0 z-10 shadow-lg"
             />
             <video
-              src="/images/about/night-video.mp4" // replace with actual video path
+              src="/images/about/about-end.mp4" // replace with actual video path
               autoPlay
               muted
               loop
-              className="absolute left-[1px] top-[0px] z-20  h-[231px] w-[106px] rounded-[18px] object-cover"
+              className="absolute left-[0px] top-[0px]   h-[232px] w-[106px] rounded-[18px] object-cover"
             />
-          </div> */}
+          </div>
         </div>
 
         {/* Smartphone Image - Positioned at the bottom-right on desktop */}
