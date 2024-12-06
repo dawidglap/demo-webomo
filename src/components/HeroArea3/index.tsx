50925221c2c0073bda5841909ddef03f5e1004bf
Dawid Glapiak <dg18@live.it>
2024-12-06 19:09:21 +0100

    still working on hero.

--- src/components/HeroArea3/index.tsx
+++ src/components/HeroArea3/index.tsx
@@ -41,7 +41,7 @@ const Hero3 = () => {
       <div className="flex h-full w-full flex-col items-center justify-evenly">
         {/* Subtitle Image with Motion */}
         <motion.div
-          className="mb-2 "
+          className="mb-2 mt-6 "
           variants={sectionVariants}
           initial="hidden"
           animate="visible"
@@ -64,13 +64,13 @@ const Hero3 = () => {
           transition={{ delay: 0.5 }}
         >
           {/* Adjust text sizes responsively */}
-          <p className="text-2xl font-bold text-black dark:text-white md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-4xl">
+          <p className="text-4xl font-bold text-black dark:text-white md:text-2xl lg:text-3xl xl:text-3xl 2xl:text-4xl 3xl:text-5xl">
             {t("mainText")}
           </p>
-          <p className="pb-0 text-lg font-light text-black dark:text-white md:text-xl lg:text-2xl xl:text-3xl 2xl:text-xl">
+          <p className="md:text-md pb-0 text-xl text-black dark:text-white lg:text-lg xl:text-lg 2xl:text-xl 3xl:text-2xl">
             {t("subText")}
           </p>
-          <span className="mt-4 bg-gradient-to-br from-[#410cd9] to-[#f68efe] bg-clip-text p-2 text-center text-2xl font-bold leading-none text-transparent md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-3xl">
+          <span className="mt-4 bg-gradient-to-br from-[#410cd9] to-[#f68efe] bg-clip-text p-2 text-center text-4xl font-bold leading-none text-transparent md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl 3xl:text-4xl">
             {t("subHeadline")}
           </span>
         </motion.div>
@@ -80,10 +80,10 @@ const Hero3 = () => {
           {/* Left Smartphone */}
           <motion.div
             className="absolute bottom-0 left-[15%] h-[99px] w-[82px] overflow-hidden border-b-8 border-black pl-10 xxs:left-[16%] xxs:flex 
-             xxs:h-[110px]  xxs:w-[91px] xs:left-[15%] 
-             xs:h-[138px] xs:w-[115px] sm:left-[14%] 
-             sm:h-[184px] sm:w-[153px] md:-left-[-18%] 
-             md:h-[229px] md:w-[192px] lg:left-[22%] lg:h-[284px] lg:w-[229px] xl:left-[25%] 2xl:left-[27%] 3xl:left-[28%] 3xl:h-[327px] 3xl:w-[264px]"
+             xxs:h-[118px]  xxs:w-[91px] xs:left-[15%] 
+             xs:h-[145px] xs:w-[115px] sm:left-[16%] 
+             sm:h-[190px] sm:w-[153px] md:-left-[-18%] 
+             md:h-[238px] md:w-[192px] lg:left-[25%] lg:h-[256px] lg:w-[206px] xl:left-[25%] xl:h-[284px] xl:w-[229px] 2xl:left-[27%] 3xl:left-[28%] 3xl:h-[327px] 3xl:w-[264px]"
             custom={1}
             initial="hidden"
             animate={smartphoneInView ? "visible" : "hidden"}
@@ -95,18 +95,19 @@ const Hero3 = () => {
               alt={t("leftMobileAlt")}
               layout="fill"
               objectFit="cover"
-              className="absolute inset-0 z-10 ml-2 md:ml-8"
+              className="absolute inset-0 z-10 ml-2  md:ml-8"
             />
           </motion.div>
 
           {/* Center Smartphone */}
           <motion.div
             ref={smartphoneRef}
-            className="relative z-[31] h-[149px] w-[107px] overflow-hidden border-b-8 border-black xxs:h-[166px] xxs:w-[119px] 
-             xs:h-[208px] xs:w-[148px] 
-             sm:h-[277px] sm:w-[198px] 
-             md:h-[346px] md:w-[248px] 
-             lg:h-[358px] lg:w-[253px]
+            className="relative z-[31] h-[149px] w-[107px] overflow-hidden border-b-8 border-black xxs:h-[172px] xxs:w-[119px] 
+             xs:h-[216px] xs:w-[148px] 
+             sm:h-[281px] sm:w-[198px] 
+             md:h-[306px] md:w-[216px] 
+             lg:h-[322px] lg:w-[227px]
+             xl:h-[358px] xl:w-[253px]
              3xl:h-[393px] 3xl:w-[278px]"
             custom={2}
             initial="hidden"
@@ -126,10 +127,10 @@ const Hero3 = () => {
           {/* Right Smartphone */}
           <motion.div
             className="absolute  bottom-0 right-[15%] h-[99px]  w-[82px] overflow-hidden border-b-8 border-black 
-            xxs:right-[16%]  xxs:flex xxs:h-[110px] 
-            xxs:w-[91px] xs:right-[15%] xs:h-[138px] 
-            xs:w-[115px] sm:right-[14%] sm:h-[184px] 
-            sm:w-[153px] md:-right-[-18%] md:h-[229px] md:w-[192px] lg:right-[22%] lg:h-[284px] lg:w-[229px] xl:right-[25%] 2xl:right-[27%]  3xl:right-[28%] 3xl:h-[327px] 3xl:w-[264px]"
+            xxs:right-[16%]  xxs:flex xxs:h-[118px] 
+            xxs:w-[91px] xs:right-[15%] xs:h-[145px] 
+            xs:w-[115px] sm:right-[16%] sm:h-[190px] 
+            sm:w-[153px] md:-right-[-18%] md:h-[238px] md:w-[192px] lg:right-[25%] lg:h-[256px] lg:w-[206px] xl:right-[25%] xl:h-[284px] xl:w-[229px] 2xl:right-[27%]  3xl:right-[28%] 3xl:h-[327px] 3xl:w-[264px]"
             custom={3}
             initial="hidden"
             animate={smartphoneInView ? "visible" : "hidden"}
