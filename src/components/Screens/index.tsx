402fde6fa97bec450b56fb19bf3cc15d7c38148e
Dawid Glapiak <dg18@live.it>
2025-03-05 23:04:36 +0100

    playing with image size

--- src/components/Screens/index.tsx
+++ src/components/Screens/index.tsx
@@ -12,7 +12,7 @@ import Graphics from "@/components/Screens/Graphics";
 // Sector images for each category
 const sectorImages = {
   "Food & Beverage": [
-    "/images/screens/check3.png",
+    "/images/screens/lastcheck.png",
     "/images/screens2/2.webp",
     "/images/screens2/3.webp",
     "/images/screens2/4.webp",
