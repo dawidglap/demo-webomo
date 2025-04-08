e977f7550eac4fbbe0e8d8e68ab7312dff350d15
Dawid Glapiak <dg18@live.it>
2025-04-08 14:08:57 +0200

    datenschutz done

--- src/app/[locale]/(site)/terms-and-conditions/page.tsx
+++ src/app/[locale]/(site)/terms-and-conditions/page.tsx
@@ -17,7 +17,7 @@ export default function Terms() {
   return (
     <div className="bg-background mx-auto my-4 mt-20 max-w-6xl px-4 py-8 text-body">
       <motion.div
-        className="border-accent mx-auto max-w-xl rounded-2xl border border-dotted px-4 py-8 shadow-lg"
+        className="border-accent mx-auto max-w-3xl rounded-2xl border border-dotted px-4 py-8 shadow-lg"
         initial="hidden"
         animate="visible"
         variants={fadeInUp}
