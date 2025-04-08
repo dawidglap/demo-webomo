cae8e060545ca0cc915bb19c8553418f062e9a52
Dawid Glapiak <dg18@live.it>
2025-04-08 14:19:18 +0200

    agb done

--- src/app/[locale]/(site)/privacy-policy/page.tsx
+++ src/app/[locale]/(site)/privacy-policy/page.tsx
@@ -11,76 +11,34 @@ export default function Privacy() {
     visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
   };
 
+  // Adjust this number according to how many entries you have
+  const termsSectionsCount = 70;
+
   return (
-    <div className="bg-background mx-auto my-4 mt-20   max-w-6xl px-4 py-8 text-body ">
+    <div className="bg-background mx-auto my-4 mt-20 max-w-6xl px-4 py-8 text-body">
       <motion.div
-        className="border-accent mx-auto max-w-xl rounded-2xl border border-dotted px-4 py-8 shadow-lg"
+        className="border-accent mx-auto max-w-3xl rounded-2xl border border-dotted px-4 py-8 shadow-lg"
         initial="hidden"
         animate="visible"
         variants={fadeInUp}
       >
-        <h1 className="text-3xl font-bold text-black dark:text-white">
+        <h1 className="text-3xl font-bold text-black dark:text-white mb-6">
           {t("title")}
         </h1>
-        <section className="mt-6">
-          {/* <h2 className="text-xl font-semibold text-black dark:text-white">
-            {t("introduction.title")}
-          </h2> */}
-          <p className="text-text-secondary mt-2 text-sm">
-            {t("introduction.description")}
-          </p>
-          <p className="text-text-secondary mt-2 text-sm">
-            {t("introduction.description2")}
-          </p>
-          <p className="text-text-secondary mt-2 text-sm">
-            {t("introduction.description3")}
-          </p>
-        </section>
 
-        {/* <section className="mt-6">
-          <h2 className="text-xl font-semibold text-black dark:text-white">
-            {t("dataCollection.title")}
-          </h2>
-          <p className="text-text-secondary mt-2 text-sm">
-            {t("dataCollection.description")}
-          </p>
-        </section>
-
-        <section className="mt-6">
-          <h2 className="text-xl font-semibold text-black dark:text-white">
-            {t("dataUse.title")}
-          </h2>
-          <p className="text-text-secondary mt-2 text-sm">
-            {t("dataUse.description")}
-          </p>
-        </section>
-
-        <section className="mt-6">
-          <h2 className="text-xl font-semibold text-black dark:text-white">
-            {t("dataSharing.title")}
-          </h2>
-          <p className="text-text-secondary mt-2 text-sm">
-            {t("dataSharing.description")}
-          </p>
-        </section>
-
-        <section className="mt-6">
-          <h2 className="text-xl font-semibold text-black dark:text-white">
-            {t("userRights.title")}
-          </h2>
-          <p className="text-text-secondary mt-2 text-sm">
-            {t("userRights.description")}
-          </p>
-        </section>
-
-        <section className="mt-6">
-          <h2 className="text-xl font-semibold text-black dark:text-white">
-            {t("cookiesPolicy.title")}
-          </h2>
-          <p className="text-text-secondary mt-2 text-sm">
-            {t("cookiesPolicy.description")}
-          </p>
-        </section> */}
+        {[...Array(termsSectionsCount)].map((_, i) => {
+          const index = i + 1;
+          return (
+            <section key={index} className="mt-6">
+              <h3 className="text-xl font-semibold text-black dark:text-white">
+                {t(`title${index}`)}
+              </h3>
+              <p className="text-text-secondary mt-2 text-sm">
+                {t(`description${index}`)}
+              </p>
+            </section>
+          );
+        })}
       </motion.div>
     </div>
   );
--- src/app/[locale]/(site)/terms-and-conditions/page.tsx
+++ src/app/[locale]/(site)/terms-and-conditions/page.tsx
@@ -4,15 +4,37 @@ import { useTranslations } from "next-intl";
 import { motion } from "framer-motion";
 
 export default function Terms() {
-  const t = useTranslations("privacy");
+  const t = useTranslations("terms");
 
   const fadeInUp = {
     hidden: { opacity: 0, y: 20 },
     visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
   };
 
-  // Adjust this number according to how many entries you have
-  const termsSectionsCount = 70;
+  // Define the sections based on the terms object in the de.json
+  const termsSections = [
+    "agreement",
+    "contractConditions",
+    "writtenForm",
+    "services",
+    "loyaltyAndConfidentiality",
+    "copyright",
+    "usageRights",
+    "illegalUse",
+    "warrantyAndDefectClaims",
+    "liabilityDisclaimer",
+    "orderReductionOrCancellation",
+    "paymentTerms",
+    "thirdPartyCosts",
+    "subcontractors",
+    "privacyAndDataProtection",
+    "acceptanceOfServices",
+    "contractDuration",
+    "termination",
+    "services2",
+    "governingLawAndJurisdiction",
+    "finalProvisions",
+  ];
 
   return (
     <div className="bg-background mx-auto my-4 mt-20 max-w-6xl px-4 py-8 text-body">
@@ -26,15 +48,14 @@ export default function Terms() {
           {t("title")}
         </h1>
 
-        {[...Array(termsSectionsCount)].map((_, i) => {
-          const index = i + 1;
+        {termsSections.map((section, index) => {
           return (
             <section key={index} className="mt-6">
               <h3 className="text-xl font-semibold text-black dark:text-white">
-                {t(`title${index}`)}
+                {t(`${section}.title`)}
               </h3>
               <p className="text-text-secondary mt-2 text-sm">
-                {t(`description${index}`)}
+                {t(`${section}.description`)}
               </p>
             </section>
           );
